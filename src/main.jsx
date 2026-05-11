5027fdf005a226cd4c31f391a551bdd498be748f
Sidhaarthan K <ksidhaarthan@gmail.com>
2026-05-11 12:02:32 +0530

    removed the vercel's speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import CartProvider from './context/CartContext.jsx'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import './index.css'
 import App from './App.jsx'
 
