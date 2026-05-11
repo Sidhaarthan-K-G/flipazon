a61df81598b5e1117911a58cb57d8a5e8a5bc236
Sidhaarthan K <ksidhaarthan@gmail.com>
2026-05-11 11:29:44 +0530

    Implemented and adding and removing functionality in cart page

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,13 +1,25 @@
 import './index.css'
 import { CartContext } from '../../context/CartContext'
 import { useContext } from 'react'
-
+import { FaPlusSquare, FaMinusSquare } from 'react-icons/fa'
 const Cart = () => {
-  const { cartItem } = useContext(CartContext)
+  const { cartItem, increaseQty, decreaseQty } = useContext(CartContext)
+
   return (
-    <div>
+    <div className="cart-container">
       {cartItem.map((item) => (
-        <p key={item.id}>{item.title}</p>
+        <div className="cart-card" key={item.id}>
+          <img src={item.images[0]} alt={item.title} className="cart-img" />
+          <div className="cart-details">
+            <p>{item.title}</p>
+            <p>${item.price}</p>
+            <div className="inc-dec">
+              <FaPlusSquare className="cart-btn" onClick={() => increaseQty(item.id)}/>
+              <FaMinusSquare className="cart-btn" onClick={() => decreaseQty(item.id)} />
+            </div>
+            <h3 className="cart-qty">qty: {item.qty}</h3>
+          </div>
+        </div>
       ))}
     </div>
   )
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,16 +1,35 @@
-import { createContext } from "react"
-import { useState } from "react"
+import { createContext } from 'react'
+import { useState } from 'react'
 export const CartContext = createContext()
 
-const CartProvider = ({children}) => {
-    const [cartItem, setCartitem] = useState([])
-    const addtocart = product => {
-        setCartitem(prev => [...prev, product])
+const CartProvider = ({ children }) => {
+  const [cartItem, setCartitem] = useState([])
+
+  const addtocart = (product) => {
+    const newProduct = {
+      ...product,
+      qty: 1,
     }
-    return(
-        <CartContext.Provider value={{cartItem, addtocart}}>
-            {children}
-        </CartContext.Provider>
+    setCartitem((prev) => [...prev, newProduct])
+  }
+  const increaseQty = (id) => {
+    setCartitem((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, qty: item.qty + 1 } : item
+      )
     )
+  }
+  const decreaseQty = (id) => {
+    setCartitem((prev) => (
+      prev.map((item) =>
+        item.id === id ? { ...item, qty: item.qty - 1 } : item
+      )
+    ))
+  }
+  return (
+    <CartContext.Provider value={{ cartItem, addtocart, increaseQty, decreaseQty }}>
+      {children}
+    </CartContext.Provider>
+  )
 }
 export default CartProvider
